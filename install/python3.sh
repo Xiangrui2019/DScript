cf61338df40e4a6e7c6346876854049974a9f5c5
VHacker2022 <xiangrui@aiursoft.com>
2022-07-15 20:20:52 +0800

    add -y

--- install/python3.sh
+++ install/python3.sh
@@ -1,4 +1,4 @@
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt update
-sudo apt install python3.8
-sudo apt install python3-pip python-is-python3
+sudo apt install -y python3.8
+sudo apt install -y python3-pip python-is-python3
