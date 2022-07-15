118afd39d3fd686c8900cb718a64e0ee37bd33f3
VHacker2022 <xiangrui@aiursoft.com>
2022-07-15 17:56:33 +0800

    use main

--- import_function.sh
+++ import_function.sh
@@ -1,4 +1,4 @@
 # 把这段代码放在需要使用的脚本最上面
-dyc() { arg="$( cut -d ' ' -f 2- <<< "$@" )" && curl -sL https://github.com/Xiangrui2019/DScript/raw/master/$1.sh | sudo bash -s $arg; }
+dyc() { arg="$( cut -d ' ' -f 2- <<< "$@" )" && curl -sL https://github.com/Xiangrui2019/DScript/raw/main/$1.sh | sudo bash -s $arg; }
 # 调用函数，例如 caddy/add_proxy
 # dyc caddy/add_proxy arg1 12345
